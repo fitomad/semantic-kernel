f73c4ef721a447eff7b5ea97d30cd52d808f0b76
fitomad <adolfo.vera@outlook.com>
2023-11-04 11:01:07 +0100

    Primera versión artículo SK

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Skills.Core;
+using Microsoft.SemanticKernel.Planning;
+using Microsoft.SemanticKernel.Connectors.AI.HuggingFace;
+using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.Extensions.Logging;
+using Globant.Plugins;
+
+var builder = new KernelBuilder();
+
+builder.WithOpenAIChatCompletionService(
+    "gpt-3.5-turbo", // OpenAI Model name
+    "sk...Ed5"       // OpenAI API Key
+);
+
+using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder
+        .SetMinimumLevel(0)
+        .AddDebug()
+        .AddConsole();
+});
+
+builder.WithLoggerFactory(loggerFactory);
+
+var kernel = builder.Build();
+
+var filePlugin = kernel.ImportSkill(new FileIOSkill(), "file");
+
+// Import the semantic functions
+var pluginsDirectory = Directory.GetCurrentDirectory();
+var orquestationPlugin = kernel.ImportSemanticSkillFromDirectory(pluginsDirectory, "Plugins");
+// Import the native functions
+var codePlugin = kernel.ImportSkill(new Statistics(), "codeStatisticsPlugin");
+
+// TEST
+
+var sourceCodeFilePath = "/Users/adolfo/Documents/Proyectos/Labs/SemanticKernel/Program.cs";
+var result = await kernel.RunAsync(sourceCodeFilePath, codePlugin["GetStatistics"]);
+
+Console.WriteLine(result);
+
---
+++ SourceCode/Statistics.cs
@@ -0,0 +1,18 @@
+using System.IO;
+using System.ComponentModel;
+using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.SkillDefinition;
+
+namespace Globant.Plugins;
+
+public sealed class Statistics 
+{
+    [SKFunction, Description("Genenate statistics for a source code file in a given path")]
+    
+    //[SKParameter("path", "The path to the source file to genenate statistics")]
+    public string GetStatistics([Description("Path to the source code file to read")] string path) 
+    {
+        var lineCount = File.ReadLines(path).Count();
+        return $"Statistics: {lineCount} lines";
+    }
+}
